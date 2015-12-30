94f49c82aaa12bc75fe05dbede89db063512fd5f
Ahmad <a.derakhshan.iut@gmail.com>
2015-12-30 23:33:00 +0330

    bonfire started

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-/***
-*
-*	SciCal
-*
-***/
+/************************************************************************
+*																		*
+*	SciCal																*
+*	A scientific calculator made by Javascript							*
+*	Author: Neo13 <a.derakhshan.iut@gmail.com>							*
+*	License: MIT														*
+*																		*
+************************************************************************/
+
+class calculator {
+/**
+ *
+ * Calculator
+ * The main class which governs the whole application
+ *
+ */
+	constructor () {
+		//
+	}
+}
